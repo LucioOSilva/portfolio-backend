1197e5d5fd33e291defb33f96928c2d946c444f2
LucioOSilva <lucioosilva00@gmail.com>
2021-08-09 23:58:21 -0300

    master: added cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = require('../routes/router');
 const mdw = require('../middlewares');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
 
 app.use(router);
